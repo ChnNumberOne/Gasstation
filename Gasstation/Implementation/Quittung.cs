b041c24450bee4f9bf3448e9a199749e1531d79b
Benjamin Zoronjic <benjamin.zoronjic2003@gmail.com>
2019-10-26 18:58:12 +0200

    -Created Quittung class -Zapfhahn nested class & functions created

---
+++ Gasstation/Implementation/Quittung.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Class1
+{
+    // saves the value of cost in CHF
+    private float receiptCost;
+
+    // date of purchase
+    private DateTime date;
+
+	public Class1(float cost, DateTime date)
+	{
+        receiptCost = cost;
+        this.date = date;
+	}
+
+    public float GetCostValue()
+    {
+        return receiptCost;
+    }
+}
--- Gasstation/Implementation/Zapfsaeule.cs
+++ Gasstation/Implementation/Zapfsaeule.cs
@@ -10,12 +10,58 @@ namespace Gasstation.Implementation
     {
         public Zapfsaeule(List<FuelTank> fuelTanks)
         {
-            this.fuelTanks = fuelTanks;
+            //this.fuelTanks = fuelTanks;
+
+            // creates new zapfhaehne and adds them to a list
+            foreach (FuelTank ft in fuelTanks) 
+            {
+                Zapfhahn zapfhahn = new Zapfhahn(ft);
+                zapfhaene.Add(zapfhahn);
+            }
         }
 
-        private List<FuelTank> fuelTanks = new List<FuelTank>();
+        //private List<FuelTank> fuelTanks = new List<FuelTank>();
+        private List<Zapfhahn> zapfhaene = new List<Zapfhahn>();
+
+        private Zapfhahn currentlySelectedZapfhahn;
+
+
 
         private FuelTank selectedFuelTank;
+
+        public void SelectZapfhahn(FuelType fuelType, int fuel) 
+        {
+            foreach (Zapfhahn zpf in zapfhaene) 
+            {
+                if (fuelType == zpf.GetFuelTank().GetFuelType()) 
+                {
+                    currentlySelectedZapfhahn = zpf;
+                }
+                else 
+                {
+                    zpf.LockZapfhahn();
+                }
+            }
+        }
+
+        public void StartTanking(int amntOfFuel) 
+        {
+            currentlySelectedZapfhahn.removeFromFuelTank(amntOfFuel);
+        }
+
+        public void FinishTanking() 
+        {
+            int drainedFuel = currentlySelectedZapfhahn.FinishFueling();
+            
+        }
+
+        public void UnlockAllZapfhaehne() 
+        {
+            foreach (Zapfhahn zpf in zapfhaene) 
+            {
+                zpf.UnlockZapfhahn();
+            }
+        }
       
 
 
@@ -32,5 +78,69 @@ namespace Gasstation.Implementation
             }
         }
 
+        // nested class Zapfhahn to access from within Zapfsaeule
+        class Zapfhahn 
+        {
+            // bool for seeing if zapfhahn can be accessed
+            private bool isLocked = false;
+
+            // fuel tank that gets accessed by this zapfhahn
+            private FuelTank fuelTank;
+
+            // saves amount of total drained fuel
+            private int drainedFuelTotal = 0;
+
+            // constructor that creates a new fueltank
+            public Zapfhahn(FuelType fuelType, int fuelCapacity)
+            {
+                fuelTank = new FuelTank(fuelType, fuelCapacity);
+            }
+
+            // constructor that uses an already created fueltank
+            public Zapfhahn(FuelTank fuelTank) 
+            {
+                this.fuelTank = fuelTank;
+            }
+
+            // locks Zapfhahn
+            public void LockZapfhahn() 
+            {
+                isLocked = true;
+            }
+
+            // unlocks Zapfhahn
+            public void UnlockZapfhahn() 
+            {
+                isLocked = false;
+            }
+
+            // changes fueltank of Zapfhahn
+            public void ChangeFuelTank(FuelTank fuelTank) 
+            {
+                this.fuelTank = fuelTank;
+            }
+
+            // removes certain amount of fuel from fueltank
+            public int removeFromFuelTank(int fuel) 
+            {
+                drainedFuelTotal += fuel;
+                fuelTank.DrainFuelFromTank(fuel);
+            }
+
+            // get total drained fuel and lock zapfhahn
+            public int FinishFueling() 
+            {
+                int totalDrained = drainedFuelTotal;
+                drainedFuelTotal = 0;
+                isLocked = true;
+                return totalDrained;
+            }
+
+            // returns fueltank
+            public FuelTank GetFuelTank() 
+            {
+                return fuelTank;
+            }
+        }
     }
 }

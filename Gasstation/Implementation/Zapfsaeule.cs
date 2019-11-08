242716d99081c88e93690855eccd96d9cfeb7450
Benjamin Zoronjic <benjamin.zoronjic2003@gmail.com>
2019-11-08 20:35:04 +0100

    -Kommentieren & Auskommentieren von Funktionen -Verbessern & Verkürzen von Funktionen

--- Gasstation/Implementation/Zapfsaeule.cs
+++ Gasstation/Implementation/Zapfsaeule.cs
@@ -33,9 +33,12 @@ namespace Gasstation.Implementation
 
         private FuelTank selectedFuelTank;
 
-        public void SelectZapfhahn(FuelType fuelType, int fuel) 
+        public void SelectZapfhahn(FuelType fuelType) 
         {
-            // Arrow Function Nach Beispiel Kassenautomat (List.Find() with custom condition)
+            // [RESOLVED]
+            // Arrow Function Nach Beispiel Kassenautomat (List.Find() with custom condition) 
+            currentlySelectedZapfhahn = zapfhaene.Find(x => x.GetFuelTank.GetFuelType() == fuelType);
+            /*
             foreach (Zapfhahn zpf in zapfhaene) 
             {
                 if (fuelType == zpf.GetFuelTank().GetFuelType()) 
@@ -46,36 +49,48 @@ namespace Gasstation.Implementation
                 {
                     zpf.LockZapfhahn();
                 }
-            }
+            }*/
         }
 
+
+        // Starts tanking from currently selected Zapfhahn
         public void StartTanking(int amntOfFuel) 
         {
             currentlySelectedZapfhahn.RemoveFromFuelTank(amntOfFuel);
         }
 
+        // [NOTE] func FinishTanking wird warscheinlich entfernt -Benji
+        // [PS:] Siehe unten bei auskommentierter func FinishFueling() fuer mehr Details
+        /*
         public void FinishTanking() 
         {
          
             int drainedFuel = currentlySelectedZapfhahn.FinishFueling();
             
-        }
+        }*/
 
+        // unlocks all zapfhaehne for after purchasing fuel
         public void UnlockAllZapfhaehne() 
         {
+            zapfhaene.ForEach(x => x.UnlockZapfhahn());
+            /*
             foreach (Zapfhahn zpf in zapfhaene) 
             {
                 zpf.UnlockZapfhahn();
             }
+            */
         }
       
 
 
-        public void SelectFuelType(string fuelTypeName)
+        public void SelectFuelType(FuelType fuelType)
         {
 
             // Kassenautomat Beispiel wie oben List.Functions können das hier einfacher lösen
             // fehlt hier ein Field?
+
+            this.selectedFuelTank = fuelTanks.Find(x => x.GetFuelType() == fuelType);
+            /*
             foreach(FuelTank currentFuelTank in fuelTanks)
             {
                 FuelType currentFuelType = currentFuelTank.GetFuelType();
@@ -84,7 +99,7 @@ namespace Gasstation.Implementation
                 {
                     this.selectedFuelTank = currentFuelTank;
                 }
-            }
+            }*/
         }
 
         // nested class Zapfhahn to access from within Zapfsaeule
@@ -139,17 +154,30 @@ namespace Gasstation.Implementation
                 return this.fuelTank.DrainFuelFromTank(fuel);
             }
 
+            //  [NOTE] func FinishFueling wird warscheinlich entfernt/nicht gebraucht. -Benji
+
+            /*
+             * @Thomas die Idee dahinter war, wenn ein Nutzer mehrere male hintereinander auftankt
+             * (weil er nacher z.B merkt, dass es nicht genug fuel ist), das er mit FinishFueling()
+             * "Bestaetigt", dass er nicht mehr tanken würde.
+             * Dies war gedacht, um eine entgueltige Quittung zu erstellen.
+             * 
+             * Die Funktion wird warscheinlich eh nacher entfernt, hatte einfach gemeint zu erklaeren,
+             * was die denkweise dahinter war.
+             */
+
             // gibt bereits den total Drained by RemoveFromFuelTank zurück als return!
             // ist der rest wirklich nötig und wenn ja muss es hier sein?
+            /*
             public int FinishFueling() 
             {
                 int totalDrained = drainedFuelTotal;
                 drainedFuelTotal = 0;
                 this.isLocked = true;
                 return totalDrained;
-            }
+            }*/
 
-            // returns fueltank
+            // returns fueltank (For information in Configuration for GUI)
             public FuelTank GetFuelTank() 
             {
                 return fuelTank;

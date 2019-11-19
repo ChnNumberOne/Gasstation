b006ebc925793f08b56ed91ab099e1f4477328af
Granstovicovis Maximillian Shrek VI <48507910+PersonCopy@users.noreply.github.com>
2019-11-19 18:09:46 +0100

    Add files via upload
    Ältere test GUI

---
+++ old code/WPF OOP Testing/WPF OOP Testing/Klasses/SavedValues.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_OOP_Testing.Klasses
+{
+    public class SavedValues
+    {
+        public static ObservableCollection<Tankstelle> tankstellen
+        {
+            get; set;
+        } = new ObservableCollection<Tankstelle>();
+        public static int TankstellenID = 0;
+        public static int ZapfsaeuleID = 0;
+        public static int TreibstoffID = 0;
+
+        public static Tankstelle FindTkstlByID(int id)
+        {
+            foreach (Tankstelle tankstelle in tankstellen)
+            {
+                if (tankstelle.id == id)
+                {
+                    return tankstelle;
+                }
+            }
+            return null;
+        }
+    }
+
+}
---
+++ old code/WPF OOP Testing/WPF OOP Testing/Klasses/Tankstelle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_OOP_Testing.Klasses
+{
+    public class Tankstelle
+    {
+        public static int newestId = 0;
+        public int id { get; set; }
+        public ObservableCollection<Zapfseule> Zapfseulen
+        {
+            get; set;
+        }
+
+        public ObservableCollection<Treibstoff> Treibstoff
+        {
+            get; set;
+        }
+
+        public string Name { get; set; }
+        public Tankstelle(string name)
+        {
+            id = SavedValues.ZapfsaeuleID;
+            SavedValues.ZapfsaeuleID++;
+            Zapfseulen = new ObservableCollection<Zapfseule>();
+            Treibstoff = new ObservableCollection<Treibstoff>();
+            Name = name;
+        }
+
+        //finds object by id
+        public static Tankstelle FindTankstelle(int id)
+        {
+            foreach (Tankstelle tk in SavedValues.tankstellen)
+            {
+                if (tk.id == id)
+                {
+                    return tk;
+                }
+            }
+            return null;
+        }
+    }
+}

b504a353f91d44055131e3066f983962fc3a9a84
Granstovicovis Maximillian Shrek VI <48507910+PersonCopy@users.noreply.github.com>
2019-11-23 17:20:50 +0100

    Add files via upload

---
+++ Gasstation/Pages/CustomerSimulation.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_OOP_Improvements.Classes;
+
+namespace WPF_OOP_Improvements.Pages
+{
+    /// <summary>
+    /// Interaktionslogik für CustomerSimulation.xaml
+    /// </summary>
+    public partial class CustomerSimulation : Page
+    {
+
+        public CustomerSimulation()
+        {
+            InitializeComponent();
+            int i = 0;
+            foreach (Zapfsaeule zapfsaeule in StaticConfig.Zapfsaeulen)
+            {
+                i++;
+                Button button = new Button()
+                {
+                    Content = zapfsaeule.Name,
+                    Name = zapfsaeule.Name.Replace(" ", "") + i,
+                    Margin = new Thickness(0, 1, 0, 1)
+                };
+                button.Click += (s, e) => 
+                {
+                    Zapfsaeule zapfsaeule1 = zapfsaeule;
+                    Button b = (Button)s;
+                    //ZapfhahnPanel.Children.Remove();
+                };
+                ZapfsaeulenPanel.Children.Add(button);
+            }
+        }
+
+        private void AddZapfhahnButton()
+        {
+
+        }
+
+        private void ZapfsaeuleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+        }
+
+            private void BackToMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.SetContent(new WelcomePage());
+        }
+
+        private void FuelToTakeOut_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Gasstation/Pages/WelcomePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_OOP_Improvements.Pages
+{
+    /// <summary>
+    /// Interaktionslogik für WelcomePage.xaml
+    /// </summary>
+    public partial class WelcomePage : Page
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+            QuickDisplayFrame.Content = new DisplayWelcome();
+        }
+
+        private void CustomerViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.SetContent(new CustomerSimulation());
+        }
+
+        private void CheckStatsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AlterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
